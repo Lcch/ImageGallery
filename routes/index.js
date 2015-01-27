3884b8f0f07265f533613e9bd793096659d8e3be
lcch <chenhao.lcch@gmail.com>
2015-01-26 16:01:31 -0800

    rewrite router

--- routes/index.js
+++ routes/index.js
@@ -8,151 +8,81 @@ router.get('/', function(req, res) {
   res.render('index', {});
 });
 
-router.get('/anime', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'anime', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('gallery', { title: 'Anime',
-                              images: filterImages}); 
-    });
-  }); 
-});
+function getGallery(category) {
+  return function(req, res) {
+            Post.getAll(function(err, images) {
+              Post.filterCategory(
+                images,
+                category.toLowerCase(),
+                function(err, filterImages) {
+                  console.log(filterImages);
+                  res.render('gallery', { title: category,
+                                          images: filterImages });
+                });
+            });
+          };
+};
+router.get('/anime', getGallery('Anime'));
+router.get('/scenery', getGallery('Scenery'));
+router.get('/daily', getGallery('Daily'));
 
-router.get('/scenery', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'scenery', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('gallery', { title: 'Scenery',
-                              images: filterImages}); 
-    });
-  }); 
-});
+function deletePhotoPage(category) {
+  return function(req, res) {
+          Post.getAll(function(err, images) {
+            Post.filterCategory(
+              images, 
+              category.toLowerCase(),
+              function(err, filterImages) {
+                console.log(filterImages);
+                res.render('delete', { title: category,
+                                       images: filterImages}); 
+            });
+          }); 
+         };
+};
+router.get('/anime/delete', deletePhotoPage('Anime'));
+router.get('/scenery/delete', deletePhotoPage('Scenery'));
+router.get('/daily/delete', deletePhotoPage('Daily'));
 
-router.get('/daily', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'daily', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('gallery', { title: 'Daily',
-                              images: filterImages}); 
-    });
-  });  
-});
+function deletePhoto(category) {
+  return function(req, res) {
+           Post.removeItem(req.body.delete, function(err) {
+             if (err) {
+               console.log(err);
+             }
+             res.redirect('/' + category.toLowerCase() + '/delete');
+           });
+         };
+};
+router.post('/anime/api/delete', deletePhoto('Anime'));
+router.post('/scenery/api/delete', deletePhoto('Scenery'));
+router.post('/daily/api/delete', deletePhoto('Daily'));
 
-
-router.get('/anime/delete', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'anime', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('delete', { title: 'Anime',
-                              images: filterImages}); 
-    });
-  }); 
-});
-router.get('/scenery/delete', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'scenery', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('delete', { title: 'Scenery',
-                              images: filterImages}); 
-    });  
-  }); 
-});
-router.get('/daily/delete', function(req, res) {
-  Post.getAll(function(err, images) {
-    Post.filterCategory(images, 'daily', function(err, filterImages) {
-      console.log(filterImages);
-      res.render('delete', { title: 'Daily',
-                             images: filterImages}); 
-    });  
-  }); 
-});
-
-
-router.post('/anime/api/delete', function(req, res) {
-  console.log(req.body.delete);
-  Post.removeItem(req.body.delete, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    res.redirect('/anime/delete');
-  });
-});
-router.post('/scenery/api/delete', function(req, res) {
-  console.log(req.body.delete);
-  Post.removeItem(req.body.delete, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    res.redirect('/scenery/delete');
-  });
-});
-router.post('/daily/api/delete', function(req, res) {
-  console.log(req.body.delete);
-  Post.removeItem(req.body.delete, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    res.redirect('/daily');
-  });
-});
-
-
-router.post('/scenery/api/photo', function(req, res) {
-  if (req.files && req.files.fileUploaded) {
-    var mimetype = req.files.fileUploaded.mimetype;
-    if (mimetype === 'image/jpeg' ||
-        mimetype === 'image/png') {
-      var post_filename = req.files.fileUploaded.name;
-      console.log(post_filename);
-      var post = new Post(post_filename, 'scenery', ['scenery']);
-      post.save(function(err) {
-        if (err) {
-          req.flash('error', err);
-          return res.redirect('/scenery'); 
-        }
-        console.log('success');
-        res.redirect('/scenery');
-      });
-    }
-  }
-});
-router.post('/anime/api/photo', function(req, res) {
-  if (req.files && req.files.fileUploaded) {
-    var mimetype = req.files.fileUploaded.mimetype;
-    if (mimetype === 'image/jpeg' ||
-        mimetype === 'image/png') {
-      var post_filename = req.files.fileUploaded.name;
-      console.log(post_filename);
-      var post = new Post(post_filename, 'anime', ['anime']);
-      post.save(function(err) {
-        if (err) {
-          req.flash('error', err);
-          return res.redirect('/anime'); 
-        }
-        console.log('success');
-        res.redirect('/anime');
-      });
-    }
-  }
-});
-router.post('/daily/api/photo', function(req, res) {
-  if (req.files && req.files.fileUploaded) {
-    var mimetype = req.files.fileUploaded.mimetype;
-    if (mimetype === 'image/jpeg' ||
-        mimetype === 'image/png') {
-      var post_filename = req.files.fileUploaded.name;
-      console.log(post_filename);
-      var post = new Post(post_filename, 'daily', ['daily']);
-      post.save(function(err) {
-        if (err) {
-          req.flash('error', err);
-          return res.redirect('/daily'); 
-        }
-        console.log('success');
-        res.redirect('/daily');
-      });
-    }
-  }
-});
+function uploadPhoto(category) {
+  return function(req, res) {
+          if (req.files && req.files.fileUploaded) {
+            var mimetype = req.files.fileUploaded.mimetype;
+            if (mimetype === 'image/jpeg' ||
+                mimetype === 'image/png') {
+              var post_filename = req.files.fileUploaded.name;
+              console.log(post_filename);
+              var post = new Post(post_filename,
+                                  category.toLowerCase(),
+                                  [category.toLowerCase()]);
+              post.save(function(err) {
+                if (err) {
+                  req.flash('error', err);
+                  return res.redirect('/' + category.toLowerCase()); 
+                }
+                console.log('success');
+                res.redirect('/' + category.toLowerCase());
+              });
+            }
+          }
+         };
+};
+router.post('/anime/api/photo', uploadPhoto('Anime'));
+router.post('/scenery/api/photo', uploadPhoto('Scenery'));
+router.post('/daily/api/photo', uploadPhoto('Daily'));
 
 module.exports = router;
